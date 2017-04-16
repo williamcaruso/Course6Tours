8c4b7292952473ecb7c223c341a2caa65355b248
Anne Kelley <akkelley@mit.edu>
2017-04-16 15:52:57 -0400

    personal info page

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@ $(document).ready(function() {
   });
   
 });
+
+//http://stackoverflow.com/questions/18503634/jquery-to-create-dynamic-textboxes-on-button-click
+$("#add_visitor").click(function() {
+
+	var newTextBox = $(document.createElement('input'))
+	     .attr("type", 'text')
+	     .attr("placeholder", "Visitor Name")
+	     .attr("size", "50")
+
+	$("<br>").appendTo("#names");
+	newTextBox.appendTo("#names");
+
+})
