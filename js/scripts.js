c200a15f209ca45f2679b227993d8280b25bac90
William Caruso <wcaruso.mit@gmail.com>
2017-04-19 11:21:48 -0400

    All finished with drag and drop

--- js/scripts.js
+++ js/scripts.js
@@ -62,42 +62,44 @@ $(document).ready(function() {
      *  https://www.w3schools.com/howto/howto_css_calendar.asp
      */
     const table=document.getElementById("calendar");
-    for(var row=0;row<9;row++){
-        var thisrow = table.insertRow(row);
-        thisrow.id ="row"+row;
-        if(row > 1) {
-            for(var day=0;day<6;day++){
-                var slot = thisrow.insertCell(day);
-                slot.id = "td"+row+day;
-                $("#td"+row+day).attr("align","center");
-                $("#td"+row+day).attr("title",days[day-1] + ' @ ' + times[row-2]);
+    if (table) {
+        for(var row=0;row<9;row++){
+            var thisrow = table.insertRow(row);
+            thisrow.id ="row"+row;
+            if(row > 1) {
+                for(var day=0;day<6;day++){
+                    var slot = thisrow.insertCell(day);
+                    slot.id = "td"+row+day;
+                    $("#td"+row+day).attr("align","center");
+                    $("#td"+row+day).attr("title",days[day-1] + ' @ ' + times[row-2]);
 
-                if(Math.random() < 0.3 && day > 0 && reserved.indexOf("td"+row+day) === -1){
-                    $("#td"+row+day).css("background-color","#D3D3D3")
-                } else {
-                    $("#td"+row+day).attr("ondrop","drop(event)");
-                    $("#td"+row+day).attr("ondragover","allowDrop(event)");
-                }
-                if(day === 0){
-                    document.getElementById('td'+row+'0').innerHTML = times[row-2];
+                    if(Math.random() < 0.3 && day > 0 && reserved.indexOf("td"+row+day) === -1){
+                        $("#td"+row+day).css("background-color","#D3D3D3")
+                    } else {
+                        $("#td"+row+day).attr("ondrop","drop(event)");
+                        $("#td"+row+day).attr("ondragover","allowDrop(event)");
+                    }
+                    if(day === 0){
+                        document.getElementById('td'+row+'0').innerHTML = times[row-2];
+                    }
                 }
             }
-        }
-        else{
-            if(row === 0){
-                var bar = thisrow.insertCell();
-                bar.id = "bar"+row;
-                $("#bar"+row).attr("align","center");
-                $("#bar"+row).attr("colspan","6");
-                document.getElementById('bar0').innerHTML = "< June >";
-            }
-            if(row === 1){
-                for(day=0;day<6;day++){
-                    slot = thisrow.insertCell(day);
-                    slot.id = "td1" + day;
-                    $("#td1"+day).attr("align","center");
-                    if(day > 0){
-                        document.getElementById('td1'+day).innerHTML = days[day-1];
+            else{
+                if(row === 0){
+                    var bar = thisrow.insertCell();
+                    bar.id = "bar"+row;
+                    $("#bar"+row).attr("align","center");
+                    $("#bar"+row).attr("colspan","6");
+                    document.getElementById('bar0').innerHTML = "< June >";
+                }
+                if(row === 1){
+                    for(day=0;day<6;day++){
+                        slot = thisrow.insertCell(day);
+                        slot.id = "td1" + day;
+                        $("#td1"+day).attr("align","center");
+                        if(day > 0){
+                            document.getElementById('td1'+day).innerHTML = days[day-1];
+                        }
                     }
                 }
             }
@@ -136,25 +138,6 @@ $("#submitPersonalInfo").click(function() {
         dataObject.handicap = handicap;
         dataObject.prefer_ee = eePref;
         dataObject.prefer_cs = csPref;
-        localStorage.setItem('personal_info_model', JSON.stringify(dataObject));
-
-    } else {
-        console.log("Unable to store form values.");
-    }
-});
-
-
-/**
- * Called when the submitDates Button is clicked
- * Saves form choices in local storage
- */
-$("#submitDates").click(function() {
-
-    if (typeof(Storage) !== "undefined") {
-        var dataObject = JSON.parse(localStorage.getItem('personal_info_model'));
-        dataObject.date1 = date1;
-        dataObject.date2 = date2;
-        dataObject.date3 = date3;
         localStorage.setItem('personal_info_model', JSON.stringify(dataObject));
 
     } else {
